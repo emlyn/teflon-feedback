923412f589f9b691dd86190e009cfd014a8e9e6d
Emlyn Corrin <emlyn@swiftkey.net>
2013-01-30 16:25:41 +0000

    Initial commit

---
+++ src/feedback/core.clj
@@ -0,0 +1,45 @@
+(ns feedback.core
+  (:require
+   [somnium.congomongo :as m]))
+
+(defn connect []
+  (let [conn (m/make-connection "lipservice"
+                                     :host "localhost"
+                                     :port 27017)]
+    (m/set-connection! conn)))
+
+(defn get-result [query id]
+  (let [results (filter #(= id (:id %))
+                        (get-in query [:response :body :results]))]
+    (if (= 1 (count results))
+      (first results)
+      (println "Uh-oh " id results))))
+
+(defn convert [event query]
+  (let [result (get-result query (:id event))]
+    {:type "binary"
+     :task "teflon-feedback"
+     :query (get-in query [:params :text])
+     :document (:id event)
+     :relevant ({"rated-good" true
+                 "rated-bad" false} (:event event))
+     :user (:who query)
+     :timestamp (:when query)
+     :url (:url result)
+     :title (:title result)
+     :score (:score result)}))
+
+(defn run []
+  (println "Total " (m/fetch-count :feedback))
+  (let [queries (m/fetch :feedback
+                         :where {:feedback {:$exists true}})]
+    (println "With feedback " (count queries))
+    (let [results (for [query queries
+                        event (:feedback query)]
+                    (convert event query))]
+      (println "Feedbacks " (count results))
+      results)))
+
+(defn -main []
+  (connect)
+  (run))
